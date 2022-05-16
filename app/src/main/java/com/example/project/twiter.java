762a426f4fcb0d9cb44d10dfbb11731de39c89c3
Sibilyov <90633901+Sibilyov@users.noreply.github.com>
2022-05-16 20:28:17 +0300

    Update twiter.java

--- app/src/main/java/com/example/project/twiter.java
+++ app/src/main/java/com/example/project/twiter.java
@@ -14,7 +14,7 @@ public class twiter extends AppCompatActivity {
         setContentView(R.layout.activity_twiter);
 
 
-n
+
             findViewById(R.id.buttonBarChart15).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
